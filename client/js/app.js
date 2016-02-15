8d4a9d88500da17d5f4e0e90ce59c2b3e5cd3462
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2016-02-15 16:24:48 -0500

    Use MOLApi factory for api calls

--- client/js/app.js
+++ client/js/app.js
@@ -1,34 +1,9 @@
 var module = angular.module('mol', [
-  //'mol.filters',
-  //'mol.services',
-  //'mol.auth',
-  //'mol.species-search',
-  //'mol.species-images',
-  //'mol.species-list',
-  //'mol.species-wiki',
-  //'mol.species-detail',
-  //'mol.consensus-map',
-  //'mol.point-filters',
-  //'mol.species-list-service',
-  //'mol.taxa-counts',
-  //'mol.location-map',
-  //'mol.loading-indicator',
-  //'mol.consensus-map',
-  //'mol.region-selector',
-  //'mol.region-model-ctrl',
   'ui.select',
   'ui.router',
   'ui-leaflet',
-  //'ui.checkbox',
-  //'imageHelpers',
-  //'leaflet-directive',
-  //'angularResizable',
-  //'nvd3',
-  //'ui-rangeSlider',
   'angular-loading-bar',
-  //'pageslide-directive',
-  //'percentage',
-  //'km2',
+  'ngResource',
   'ngSanitize',
   'ngCookies',
   'mol.inventory-controllers'
@@ -81,3 +56,19 @@ module.directive('molWindowResize', function ($window) {
       });
     };
 });
+
+module.factory('MOLApi', ['$http', function($http) {
+		return function(service, params, canceller, loading) {
+			loading = (typeof loading === undefined) ? false : loading;
+			return $http({
+				method:'GET',
+				url: 'https://api.mol.org/0.x/{0}'.format(service),
+				params: params,
+				withCredentials: false,
+				cache: true,
+				timeout: (canceller) ? canceller.promise : undefined,
+				ignoreLoadingBar: loading
+			});
+		};
+	}
+]);
--- client/views/inventory/controller.js
+++ client/views/inventory/controller.js
@@ -1,8 +1,8 @@
 var module = angular.module('mol.inventory-controllers', []);
 
 module.controller('inventoryCtrl',
-    ['$scope', 'leafletData', '$timeout', '$window', '$http',
-    function($scope, leafletData, $timeout, $window, $http) {
+    ['$scope', 'leafletData', '$timeout', '$window', '$http', 'MOLApi',
+    function($scope, leafletData, $timeout, $window, $http, MOLApi) {
   $scope.option = {};
 
   $scope.map = {
@@ -10,18 +10,16 @@ module.controller('inventoryCtrl',
     events: { map: { enable: ['click'], logic: 'emit' } }
   };
 
-  // TODO: Use MOLApi here
-  $http.get('https://api.mol.org/0.x/inventory/datasets', { withCredentials: false })
-    .then(function(response) {
-      $scope.data = response.data;
-    }, function(response) {
-      console.log('error', response);
-    });
+  MOLApi('inventory/datasets').then(function(response) {
+    $scope.data = response.data;
+  });
 
   // TODO: Use component helper controller.js
   $scope.windowResize = function(size) {
     leafletData.getMap().then(function(map) {
-      if (!size) return;
+      if (!size) {
+        size = { 'h': window.innerHeight, 'w': window.innerWidth};
+      }
       $timeout(function() {
         var selector = '.mol-inventory-map .leaflet-container',
             footer   = angular.element('footer').height(),
